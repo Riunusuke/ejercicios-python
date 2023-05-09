b92943820174163fa293bb1380904b36c87c6601
Andres Diaz <diazandresfelipe91@gmail.com>
2023-05-09 12:06:45 -0500

    Add web-server example with FastAPI

---
+++ web-server/main.py
@@ -0,0 +1,25 @@
+import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact')
+def get_list():
+    return {'name': 'Riunusuke'}
+
+@app.get('/pagina', response_class=HTMLResponse)
+def get_page():
+    return """
+    <h1> Hola </h1>
+"""
+
+def run():
+    store.get_categories()
+    
+if __name__ == "__main__":
+    run()
